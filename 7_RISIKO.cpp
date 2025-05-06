8301f7dc35bc72b202ca374f3f65443fa2a5a294
norismichele4FS <michele.noris@studenti.liceomascheroni.it>
2025-05-06 18:24:47 +0200

    Aggiungere i file di progetto.

---
+++ 7_RISIKO.cpp
@@ -0,0 +1,26 @@
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+	// Create the main window
+	sf::RenderWindow window(sf::VideoMode({ 800, 800 }), "SFML window");
+
+	// Start the game loop
+	while (window.isOpen())
+	{
+		// Process events
+		while (const std::optional event = window.pollEvent())
+		{
+			// Close window: exit
+			if (event->is<sf::Event::Closed>())
+				window.close();
+		}
+
+		// Clear screen
+		window.clear();
+
+		// Update the window
+		window.display();
+	}
+}
