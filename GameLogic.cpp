33a7fc72da93878bae4d7030976428b5cf6b506a
norismichele4FS <michele.noris@studenti.liceomascheroni.it>
2025-05-27 11:55:03 +0200

    fix getNumTerritori

--- GameLogic.cpp
+++ GameLogic.cpp
@@ -79,9 +79,10 @@ bool GameLogic::sonoConfinanti(vector<pair<string, string>>& confiniTerritori, s
 
 int GameLogic::getNumTerritori(Player& giocatore, int nArmateMinime, vector<Territorio> territori) {
 	int tempNumArmate = 0;
-	for (int i = 0; i < giocatore.getTerritori().size(); i++)
+	for (int i = 0; i < territori.size(); i++)
 	{
-		if (territori[i].getNumArmate() >= nArmateMinime) {
+		if (territori[i].getIdConquistatore() == giocatore.getIdGiocatore() &&
+			territori[i].getNumArmate() >= nArmateMinime) {
 			tempNumArmate++;
 		}
 	}
